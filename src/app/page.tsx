64656e7e05d8c865e0f1ae5422874dc28d3e7b2a
Don Artkins <opiyodon9@gmail.com>
2023-08-31 23:25:23 +0300

    second update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <main className="h-screen bg-black">
-      <label className ="text-white p-6 text-3xl">Trial</label>
+      <label className ="text-white p-6 text-3xl">Trial Updated</label>
     </main>
   )
 }
